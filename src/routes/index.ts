78fff2c2258b53205f09ae220a789bb3f2e79c6e
naufalandya <135545118+naufalandya@users.noreply.github.com>
2024-05-05 14:21:12 +0700

    first commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,2 @@
-export { default as userRoute } from "./user.route"
-export { default as authRoute } from "./auth.route"
-export { default as apiRoute } from "./api-doc.route"
 export { default as mediaRoute} from "./media.route"
 export { default as imageKitRoute} from "./imagekit.route"
